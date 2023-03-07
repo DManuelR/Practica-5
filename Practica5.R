34facc3eb3696a832d6a5130387d5de6f6986be8
DManuelR <davidvillaresi@gmail.com>
2023-03-07 12:49:15 +0100

    Update Practica5.R

--- Practica5.R
+++ Practica5.R
@@ -5,3 +5,18 @@ numArtefactos_int <- as.integer(numArtefactos)
 
 typeof(numArtefactos_int)
 
+mean(numArtefactos_int)
+
+median(numArtefactos_int) #Es el número intermedio de un grupo de números.  La mitad de los números son superiores a la mediana y la mitad de los números tienen valores menores que la mediana.
+
+moda <- function(x) 
+{u <- unique(x)
+  tab <- tabulate(match(x, u))
+  u[tab == max(tab)]} 
+moda(numArtefactos_int)
+
+tabulate(numArtefactos_int, nbins = 102)
+
+quantile(numArtefactos_int)
+
+
