6014992596128cbc6fa0620bedfeed168e23c7b5
DManuelR <davidvillaresi@gmail.com>
2023-03-07 11:57:34 +0100

    Update Practica5.R

--- Practica5.R
+++ Practica5.R
@@ -1,1 +1,7 @@
-#hola
+numArtefactos <- c(17, 54, 10, 34, 90, 33, 49, 82, 12, 23, 56, 78, 44, 102, 10, 53, 4, 28, 37, 95)
+numArtefactos
+
+numArtefactos_int <- as.integer(numArtefactos)
+
+typeof(numArtefactos_int)
+
