be14f12a693b2e250ff7e359707db7b4f3e3fca6
DManuelR <davidvillaresi@gmail.com>
2023-03-11 17:54:48 +0100

    Update Practica5.R

--- Practica5.R
+++ Practica5.R
@@ -15,8 +15,31 @@ moda <- function(x)
   u[tab == max(tab)]} 
 moda(numArtefactos_int)
 
-tabulate(numArtefactos_int, nbins = 102)
+tabulate(numArtefactos_int, nbins = 102) #El valor correspondiente de la moda se repite 2 veces
 
 quantile(numArtefactos_int)
 
+rango_intercuartílico <- IQR(numArtefactos_int)
+rango_intercuartílico
+
+
+rango_artefactos <- diff(range(numArtefactos_int))
+rango_artefactos
+
+var_val <- var(numArtefactos_int)
+var_val
+var_val2 <- sum((numArtefactos_int - mean(numArtefactos_int))^2)/(length(numArtefactos_int) - 1)
+var_val2
+
+desviacion_estandar <- sd(numArtefactos_int)
+desviacion_estandar
+desviacion_estandar2 <- sqrt(sum((numArtefactos_int - mean(numArtefactos_int))^2)/(length(numArtefactos_int) - 1))
+desviacion_estandar2
+
+#11, ...
+
+library(ggplot2)
+
+Grafica_dispersion <- plot(numArtefactos_int, y=rep(0, length(numArtefactos_int)), pch=19, xlim=c(min(numArtefactos_int) - 1, max(numArtefactos_int) + 1), xlab="Valor", main="Dispersión de numArtefactos_int")
+abline(v=mean(numArtefactos_int), col="red", lty=2)
 
