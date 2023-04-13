165edb005b8423ff6e3ef9bd9d1036cd5bf2e4d3
DManuelR <davidvillaresi@gmail.com>
2023-04-13 18:27:38 +0200

    Create Prácticas1-4.R

---
+++ PrácticasTemas1-4/Prácticas1-4.R
@@ -0,0 +1,5 @@
+##Ej 1. Las Variables MAT, CON, LOO, PEG son nominales.La variable COND es ordinal. La variable DATE es interval. Las variables MAXLE, SOCLE, MAXWI, UPSOC, LOSOC, MAWIT, WEIGHT son ratio.
+##Ej 2. 
+##Ej 3. Tabla importada
+##Ej 4. 
+colnames(spearheads) <- c("Caso", "Material", "Contexto", "Loop", "Remache", "Conservacion", "Fecha", "Long_max", "Long_cubo", "Ancho_max", "Ancho_cubo_inf", "Long_ancho_max", "Peso")
